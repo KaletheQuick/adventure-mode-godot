2c70728f957240f5e40d2474dc21b162a7f6297a
KaletheQuick <thomashaynes@eastereggproductions.com>
2024-02-02 00:00:54 -0800

    add animated char turing

--- scripts/actor.gd
+++ scripts/actor.gd
@@ -7,7 +7,20 @@ const JUMP_VELOCITY = 6.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+@export var mainCam : Camera3D 
 
+var dot : MeshInstance3D
+
+func _ready():
+	
+	var sphere = SphereMesh.new()
+	sphere.radial_segments = 7
+	sphere.radius = 0.125
+	sphere.height = 0.25
+	dot = MeshInstance3D.new()
+	dot.mesh = sphere
+	get_tree().root.add_child.call_deferred(dot)
+	dot.name = "~dot~"
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,7 +33,7 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_down","ui_up")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
@@ -28,6 +41,34 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+	print(input_dir)
+	# Figuring out relative movement
+	var mv_z = -mainCam.global_transform.basis.z
+	mv_z.y = 0
+	mv_z = mv_z.normalized()
+	mv_z = mv_z * input_dir.y
+	var mv_x = mainCam.global_transform.basis.x
+	mv_x.y = 0
+	mv_x = mv_x.normalized()
+	mv_x = mv_x * input_dir.x
+	var go_dir = (mv_x + mv_z)
+
+	var animation_tree = $AnimationTree
+	animation_tree.set("parameters/BlendSpace2D/blend_position", Vector2(( global_basis.inverse() * -go_dir).x,-( global_basis.inverse() * -go_dir).z))
+	print(global_basis.inverse() * -go_dir)
+	# Get the motion delta.
+	velocity = ((animation_tree.get_root_motion_rotation_accumulator().inverse() * get_quaternion()) * animation_tree.get_root_motion_position() / delta) * 2
+	# global_basis * (animation_tree.get_root_motion_position_accumulator())
+	quaternion = quaternion * ((animation_tree.get_root_motion_rotation() / delta) * 10)
+	animation_tree.get_root_motion_scale()
+	
+	dot.global_position = global_position + go_dir
+	
+
+	# Get the actual blended value of the animation.
+	animation_tree.get_root_motion_position_accumulator()
+	animation_tree.get_root_motion_rotation_accumulator()
+	animation_tree.get_root_motion_scale_accumulator()
 
 	move_and_slide()
 	_TEMPORARY_fall_death()
