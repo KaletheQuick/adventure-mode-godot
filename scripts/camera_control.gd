e8a13dd1f20eb137929e3558155d0290d9e3c313
Adem <ademosmancevic7@gmail.com>
2024-02-09 14:36:50 -0800

    Zoom in func _process
    I followed the suggestions and made the zoom in _process to use delta.

--- scripts/camera_control.gd
+++ scripts/camera_control.gd
@@ -2,11 +2,11 @@ extends Camera3D
 
 @export var target_current : Node3D
 @export var distance = 5.0
-
 var distance_to_target: float
-var target_zoom = Vector3(0,0,0)
-var zoom_speed = 0.01
-var initial_cam_position = Vector3(global_transform.origin)
+@export var zoom_min : float  
+@export var zoom_max : float 
+@export var zoom_target = 1
+@export var zoom_speed = 0.4
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,24 +17,16 @@ func _ready():
 func _process(delta):
 	if is_instance_valid(target_current):
 		look_at(target_current.global_position)
-		var target_position = target_current.global_transform.origin
-		var distance_to_target = target_current.global_transform.origin.distance_to(global_transform.origin)
-		if distance_to_target > 8.0:
-			global_transform.origin = target_current.global_transform.origin - target_current.global_transform.basis.z
-			global_transform.origin.y = 3
-			
-
-func _input(event):
-	if event is InputEventKey:
-		var key_event = event as InputEventKey 
-		if Input.is_action_just_pressed("zoom"):
-			var pos = global_transform.origin
-			target_zoom += Vector3(0,0,-5)
-			global_transform.origin = pos.lerp(target_zoom,zoom_speed)
-			if target_zoom.z > -15:
-				target_zoom == Vector3(0,0,0)
 		
-	if Input.is_action_just_pressed("cam_shift"):  # staticcamera movemnt via keyboard input
-		print("V pressed")
-	
-	
+		var target_position = target_current.global_transform.origin
+		var zoom_square = zoom_target ^ 2
+		target_position.y += 3
+		var distance_to_target = target_current.global_transform.origin.distance_to(global_transform.origin)
+		var distance_difference = zoom_square - target_position.y
+		if Input.is_action_just_pressed("zoom"):
+			zoom_target += -15
+			global_transform.origin += -global_basis.z * distance_difference * zoom_speed * delta
+			
+		if distance_to_target > 8.0 or distance_to_target < 2.0 :
+			global_transform.origin = target_current.global_transform.origin - target_current.global_transform.basis.z
+			global_transform.origin.y = target_position.y
