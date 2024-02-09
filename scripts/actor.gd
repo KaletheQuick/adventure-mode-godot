6bed685f38928360aca5b1305fbcbed4aeb55fa2
KaletheQuick <thomashaynes@eastereggproductions.com>
2024-02-09 02:02:20 -0800

    add thrall and socket control and animations

--- scripts/actor.gd
+++ scripts/actor.gd
@@ -7,20 +7,12 @@ const JUMP_VELOCITY = 6.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-@export var mainCam : Camera3D 
 
-var dot : MeshInstance3D
+
+var desired_move = Vector3.ZERO
 
 func _ready():
-	
-	var sphere = SphereMesh.new()
-	sphere.radial_segments = 7
-	sphere.radius = 0.125
-	sphere.height = 0.25
-	dot = MeshInstance3D.new()
-	dot.mesh = sphere
-	get_tree().root.add_child.call_deferred(dot)
-	dot.name = "~dot~"
+	pass
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -32,37 +24,16 @@ func _physics_process(delta):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_down","ui_up")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	print(input_dir)
-	# Figuring out relative movement
-	var mv_z = -mainCam.global_transform.basis.z
-	mv_z.y = 0
-	mv_z = mv_z.normalized()
-	mv_z = mv_z * input_dir.y
-	var mv_x = mainCam.global_transform.basis.x
-	mv_x.y = 0
-	mv_x = mv_x.normalized()
-	mv_x = mv_x * input_dir.x
-	var go_dir = (mv_x + mv_z)
 
 	var animation_tree = $AnimationTree
-	animation_tree.set("parameters/blend_position", Vector2(( global_basis.inverse() * -go_dir).x,-( global_basis.inverse() * -go_dir).z))
-	print(global_basis.inverse() * -go_dir)
+	animation_tree.set("parameters/blend_position", Vector2(( global_basis.inverse() * -desired_move).x,-( global_basis.inverse() * -desired_move).z))
+	print(global_basis.inverse() * -desired_move)
 	# Get the motion delta.
 	velocity = ((animation_tree.get_root_motion_rotation_accumulator().inverse() * get_quaternion()) * animation_tree.get_root_motion_position() / delta) * 2
 	# global_basis * (animation_tree.get_root_motion_position_accumulator())
 	quaternion = quaternion * ((animation_tree.get_root_motion_rotation() / delta) * 10)
 	animation_tree.get_root_motion_scale()
 	
-	dot.global_position = global_position + go_dir
 	
 
 	# Get the actual blended value of the animation.
@@ -72,6 +43,9 @@ func _physics_process(delta):
 
 	move_and_slide()
 	_TEMPORARY_fall_death()
+
+func handle_movement(movement : Vector3):
+	desired_move = movement
 
 
 # Resets position to origin
--- scripts/playerSocket_adventure.gd
+++ scripts/playerSocket_adventure.gd
@@ -5,13 +5,29 @@ extends Node
 # Thralls take same inputs, attack, desired move direction, etc
 # Easily swap socket type, ex player switch to another character
 # or someone comes over the network and controlls something
+# Made by KaletheQuick
 
-@export var thrall : Node3D # the thing to be controlled 
-# Would currently be nothing, but will be actor.gd
+@export var thrall : CharacterBody3D # the thing to be controlled 
+@export var player_prefix = "p1_" # used for local multiplayer
+@export var test_second_thrall : CharacterBody3D
+var primary_thrall = true
 
-# Called when the node enters the scene tree for the first time.
+@export var mainCam : Camera3D 
+
+var dot : MeshInstance3D # debug
+
 func _ready():
 	pass # Replace with function body.
+	
+	#debug
+	var sphere = SphereMesh.new()
+	sphere.radial_segments = 7
+	sphere.radius = 0.125
+	sphere.height = 0.25
+	dot = MeshInstance3D.new()
+	dot.mesh = sphere
+	get_tree().root.add_child.call_deferred(dot)
+	dot.name = "~dot~"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -20,8 +36,40 @@ func _process(delta):
 		return
 	_collect_inputs(delta)
 
+	# SECTION Debug test of moving to new thralls
+	if Input.is_action_just_pressed("p1_dodge"):		
+		primary_thrall = !primary_thrall
+		if primary_thrall: 
+			mainCam.target_current = thrall
+		else:
+			mainCam.target_current = test_second_thrall
+	# !SECTION
+
 func _collect_inputs(delta):
 	pass 
 	# TODO - Collect player input
 	# 2    - Change to relevant stuff
 	# 3    - Pass forward desired inputs to thrall 
+
+	var input_dir = Input.get_vector(player_prefix + "move_left", player_prefix + "move_right", player_prefix + "move_dn", player_prefix + "move_up")
+
+	# Figuring out relative movement
+	var mv_z = -mainCam.global_transform.basis.z
+	mv_z.y = 0
+	mv_z = mv_z.normalized()
+	mv_z = mv_z * input_dir.y
+	var mv_x = mainCam.global_transform.basis.x
+	mv_x.y = 0
+	mv_x = mv_x.normalized()
+	mv_x = mv_x * input_dir.x
+	var go_dir = (mv_x + mv_z)
+
+	go_dir.y = Input.get_axis(player_prefix + "crouch", player_prefix + "jump")
+	
+	if primary_thrall: 
+		thrall.handle_movement(go_dir)
+	else:
+		test_second_thrall.handle_movement(go_dir)
+
+
+	dot.global_position = thrall.global_position + go_dir
