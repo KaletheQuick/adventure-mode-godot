f3d9d429590e231df784b7e395f32d9d981d7f78
KaletheQuick <thomashaynes@eastereggproductions.com>
2024-04-11 13:12:00 -0700

    add swim and modify to remove conflict with climb

--- Modular Mobility/mvpk_areaDependant.gd
+++ Modular Mobility/mvpk_areaDependant.gd
@@ -3,7 +3,7 @@ extends MovementPackage
 class_name mvpk_area_dep
 
 @export var area_type = "water"
-
+#@export_multiline var movement_extra_expression : String
 
 var LDT = 0.1 # last delta time, some error or something, idk
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -28,6 +28,25 @@ func move_thrall(thrall : Actor, delta : float):
 	# Get the motion delta.
 	thrall.velocity = ((thrall.animation_tree.get_root_motion_rotation_accumulator().inverse() * thrall.get_quaternion()) * thrall.animation_tree.get_root_motion_position() / thrall.LDT) * 2
 
+
+#	if movement_extra_expression != "":
+#		var expression = Expression.new()
+#		#expression.
+#		expression.parse(movement_extra_expression)
+#		expression.execute([],self)
+	# NOTE - Not specifically 'area dependant movement'
+	# NOTE - This is specific to swimming
+	var shape = thrall.get_node("water_cast") #NOTE Stand in situation gantry
+	var bouyancy = 0
+	for x in range(shape.get_collision_count()):
+		#print(shape.get_collider(x).name)
+		if shape.get_collider(x).name == area_type:
+			bouyancy = shape.global_position.y - shape.get_collider(x).get_parent().global_position.y
+			print(bouyancy)
+			thrall.velocity.y = bouyancy + 0.05
+			thrall.velocity.y = clampf(thrall.velocity.y, 0.5, -0.5)
+
+
 	# Add the gravity.
 	if not thrall.is_on_floor():# && desired_move.y < 0.1:
 		pass
@@ -42,3 +61,13 @@ func move_thrall(thrall : Actor, delta : float):
 	thrall.quaternion = thrall.quaternion * ((thrall.animation_tree.get_root_motion_rotation() / delta) * 10)
 
 	thrall.move_and_slide()
+
+
+
+
+
+
+
+
+
+
