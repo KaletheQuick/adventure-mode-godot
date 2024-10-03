27312286853606a68ed8d152c11ae2aaa5ac4b5d
KaletheQuick <thomashaynes@eastereggproductions.com>
2024-10-03 09:13:06 -0700

    Add Godette

--- scripts/cam_gantry_playerFollow.gd
+++ scripts/cam_gantry_playerFollow.gd
@@ -18,7 +18,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if thrall == null:
 		return
 	var desired_pos = thrall.global_position
--- scripts/camera_control.gd
+++ scripts/camera_control.gd
@@ -44,7 +44,7 @@ func _ready():
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	#return
 	#print(str(get_viewport().size) + ", " + str(get_window().size))
 	
